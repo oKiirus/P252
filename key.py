6c3f0edfceb5ccd3e075104bd2676838f7e062e0
oKiirus <77072551+oKiirus@users.noreply.github.com>
2022-05-27 20:46:08 -0700

    Add files via upload

---
+++ key.py
@@ -0,0 +1,12 @@
+creds = {
+  "type": "service_account",
+  "project_id": "weather-monitoring-syste-852c1",
+  "private_key_id": "89ea8ee2989b66139ce3cee8a2d665fc22884141",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDpdFsvFMJtnIf+\nteJWqOnqJ+2KDa1kJgsFrTEbH0QgIchM4MSAE2DabcCHCgNNZsJ0k7KwzfDhrLKV\nSI+ywGEHwcnKTK/WEFXEbTOvqanOZhISS/PMg07LpSA8uLLlxFzkWq1dLMGQBFv8\nfMh+hB/VPfBvv8fAvMDSKbJXeaLkaCKmjQAW28GhNG3CTtETaHipzgja5yDNepe4\n4m4B/+eycdq4nUV8vMEQo8af5U02UfoJFJ8AD9MYrXjN/NhZPQK7qw/ntF7uA0k4\n4g68oRN9SuEA2mjqyfdrsmaDWVLlnsKYIEGh9nDKw25FYC6c/KnYiq3oHgsFmAW/\nTyuQZx/bAgMBAAECggEATGMqQumEBOGnDgztlnWGtHuEGhwHlDFugdj9rGY6oFjt\ndVpGcbHlixNGX+lC01zAW+zIF1oLa4xaksY/XrncBfG+2VbmYiDOL4ub0equCT8U\nwq/fghek/LV3Kuc0gkISMxYIi+hNHZaPkrox0FPjJp8xwR77eXFJlnrWRBEk358j\nWLKexc8gSpCwsImUb9WmVqHS5BbgWc60r1ABlgUWghXsXtkcqwbijhz+SKbucjoS\nAwjw+N4nXQt+9T6J2X0GITfOMyv787KLHNJMpV5Fqd0A3q+eKI+bVBasVxVTULJP\njd5p88sojP/pUaI2uXuPEuU8+g/HG82b2KZzbq9FAQKBgQD3UMnVTmPMtlXj2s2Y\nGjN4dMcxP52Hw+GLe/G+2cLTfsUFQJo6FqVNNbBDk2dSYlB+tiNOnWRgCyE9fupa\nEHz+Vnj7m6zmUL9iQVjTC9BltMl/h8BHF9xwHjJIpPuxm+XmpEX8ZZtRcLSQ8jp4\nfc12aehzQqWnkKhqdfP9ZH13IQKBgQDxpvcs+XzNZ/fzqTVtlGCGYz++qDUPHugY\niLb9HMs7LXZRtgY/7LXsavo9gHfIY03h7jKjmJetXjcKo/U7YgZD7UQ7axcASEi9\nSWoUxstLXTjP+b2scloF5c5s4K+LWHVJZg12iZsbPkK+XOT0fWuNm1+2MJyF5CR4\n+TNOZAmDewKBgQD3CFVyoUiClKaaYFX4VkOPUGYfRUHe+MPzhaxlTDKU+w1lP7BN\n0Yzv4G7eMQCFg/qPdvMxdXlrjfJ38Ku5prqTemCCQm+j5ogkZs2IkUZqMPkMT9a5\nrJXdz+u+MD3QnL45cWEOyZco9AEIv0u0S0TNwOAVMT8ybwYqwQMj2hqQwQKBgQDM\nCshAwHSB2B0EL8b+PL/4GUuUx+DtYJJnTOD9/dcYo715JzJFvbCq7HVZguUhXcBA\nWemGO20vJRNit6h0uMNDjc8EQNtOELrioV+iJW04uIElEuH1/yRGQJG5W0q+ZTW7\ngSEzCuPd8AdZR4THcDNlgWEirA8t+sJVJ04MDen+iwKBgQCclW2zfHU7WKxuBI0H\noQX4i6DS0rMXU3W75EwR38wun+FqYYa+Pl2M42WyeQHFDtsYap0Y0E4+VOy65zul\n+fIVzNM7AAKwb3MfhpkbRXx3N8cNrqzAvKL9W3gn/WzpJclGDZ1kPtQLH68JkwP1\nnPcRj+Vp4xdaYm1fKmOygqYKCw==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-yvh8q@weather-monitoring-syste-852c1.iam.gserviceaccount.com",
+  "client_id": "104203946046437124697",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-yvh8q%40weather-monitoring-syste-852c1.iam.gserviceaccount.com"
+}
---
+++ main.py
@@ -0,0 +1,47 @@
+from xml.dom.minidom import Document
+from flask import Flask, jsonify, render_template, request
+import os
+import datetime
+from firebase_admin import credentials, initialize_app, firestore
+from key import creds
+import firebase_admin
+
+app = Flask(__name__)
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(creds)
+    default_app = initialize_app(cred)
+
+firebase_db = firestore.client()
+
+@app.route("/add-data", methods=["POST"])
+def add_data():
+    try:
+        temperature = request.json.get("temperature")
+        humidity = request.json.get("humidity")
+        altitude = request.json.get("altitude")
+        pressure = request.json.get("pressure")
+        document_ref = firebase_db.collection("data")
+        add_values = document_ref.document().create(dict(temperature=temperature, humidity=humidity, altitude=0, pressure=0, date=datetime.datetime.utcnow()))
+        return jsonify({
+            "status": "success"
+        }), 201
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        }), 400
+
+@app.route("/")
+def index():
+    try: 
+        document_ref = firebase_db.collection("data") 
+        data = document_ref.order_by("date", direction='DESCENDING').limit(1).get()[0].to_dict() 
+        return render_template("/home/home.html", data=data) 
+    except Exception as e: 
+        print(str(e))
+        return jsonify({ "status": "error", "message": "No data in database yet!" }), 400
+   
+if __name__ == '__main__':
+    #app.run(debug=True)
+    app.run(host='10.0.0.32', port=5000)
